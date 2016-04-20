1df4e2dc38b0b05a22324cdda92088d3b15d58c2
micky387 <mickaelsaibi@free.fr>
2016-04-20 14:39:54 +0200

    First release Samsung Firmware Note edge N915FXXU1DPC7 Marshmallow

---
+++ build_kernel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export ARCH=arm
+#export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=$(pwd)/arm-eabi-4.8/bin/arm-eabi-
+mkdir output
+
+make -C $(pwd) O=output apq8084_sec_defconfig VARIANT_DEFCONFIG=apq8084_sec_tblte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output
+
+cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
