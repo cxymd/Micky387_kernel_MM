8fce650dabd0832188c46ce0ce074cc01843b7ce
micky387 <mickaelsaibi@free.fr>
2016-04-21 18:40:50 +0200

    Build_kernel: update script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 export ARCH=arm
-#export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.8/bin/arm-eabi-
-export CROSS_COMPILE=$(pwd)/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=/home/micky387/android/kernel/Toolchain/bin/arm-cortex_a15-linux-gnueabihf-
 mkdir output
 
-make -C $(pwd) O=output apq8084_sec_defconfig VARIANT_DEFCONFIG=apq8084_sec_tblte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output
+make -C $(pwd) O=output VARIANT_DEFCONFIG=micky387_defconfig empty_defconfig
+make -j5 -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
